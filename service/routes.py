cd8fd109b15ce90b37373f3acbb98d6f0065880f
maciel-j <jose.s.maciel@outlook.com>
2024-11-18 10:44:33 -0500

    add list feature

--- service/routes.py
+++ service/routes.py
@@ -61,8 +61,17 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """Returns a list of Accounts"""
+    app.logger.info("Request to list Accounts...")
 
-# ... place you code here to LIST accounts ...
+    app.logger.info("Find all")
+    accounts = Account.all()
+
+    results = [account.serialize() for account in accounts]
+    app.logger.info("[%s] Accounts returned", len(results))
+    return jsonify(results), status.HTTP_200_OK
 
 
 ######################################################################
